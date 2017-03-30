9829805fcda7abbb60d195e90fd6628d1976e7b6
Chris Grabosky <chris@grabosky.net>
2017-03-30 13:14:01 -0400

    2017 draft product page

--- products.js
+++ products.js
@@ -10,12 +10,15 @@ jsonpCallback({"products":[
 	{"id":9,"name":"Onion Rings","price":"3.50","itemType":"food"},
 	{"id":10,"name":"Poppers","price":"4.00","itemType":"food"},
 	{"id":11,"name":"Wings","price":"9.00","itemType":"food"},
-	{"id":12,"name":"Guinness Draft","price":"5.00","itemType":"draft"},
-	{"id":13,"name":"Miller Lite Draft","price":"4.00","itemType":"draft"},
-	{"id":14,"name":"Yuengling Draft","price":"4.00","itemType":"draft"},
+	//{"id":12,"name":"Guinness Draft","price":"5.50","itemType":"draft"},
+	{"id":23,"name":"Craft","price":"5.00","itemType":"draft"},
+	{"id":13,"name":"Miller Lite","price":"4.00","itemType":"draft"},
+	{"id":24,"name":"Smmr Shandy","price":"4.00","itemType":"draft"},
+	{"id":14,"name":"Yueng. Lager","price":"4.00","itemType":"draft"},
 	{"id":15,"name":"Blue Moon","price":"4.00","itemType":"bottle"},
 	{"id":16,"name":"Corona","price":"4.00","itemType":"bottle"},
-	{"id":17,"name":"Shandy","price":"4.00","itemType":"bottle"},
+	{"id":25,"name":"Twisted Tea","price":"4.00","itemType":"bottle"},
+	//{"id":17,"name":"Shandy","price":"4.00","itemType":"bottle"},
 	{"id":18,"name":"Wine","price":"4.00","itemType":"bottle"},
 	{"id":19,"name":"Coors NA","price":"2.50","itemType":"na"},
 	{"id":20,"name":"Root Beer","price":"1.50","itemType":"na"},
@@ -26,6 +29,6 @@ jsonpCallback({"products":[
     "eventname":"St. Charles Carnival",
     "website":"www.scbcarnival.com",
     "logo":"logo.png",
-    "eventdate":"June 13-18, 6-11pm nightly"
+    "eventdate":"June 12-17, 6-11pm nightly"
     });
 
