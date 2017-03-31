27d14b5fed7b0cdd39ca6101c5e2511979a41ca3
Chris Grabosky <chris@grabosky.net>
2017-03-31 09:59:35 -0400

    Readme updates and making config more friendly

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ All menu items are listed in the products.js and then it builds the dynamic touc
 Very basic.
 
 Built on BootStrap
+
+# Modifying 
+Simply edit the products.js. Create a list of objects as shown in the sample. The ID must be unique for reciept purposes.
+
+Each product must also have a year key. This again is for historical data purposes. Only products with a year matching the universal config section at the end are shown.
+
+The universal config section brands the site and reciept.
--- products.js
+++ products.js
@@ -1,4 +1,6 @@
 jsonpCallback({"products":[
+	// price list, ID not important, just must be unique.
+	// order in list below is order on page
 	{"id":1,"name":"Clams (6)","price":"3.50","itemType":"food", "years":[2016,2017]},
 	{"id":2,"name":"Crab Cake","price":"6.00","itemType":"food", "years":[2016,2017]},
 	{"id":3,"name":"Fried Calamari","price":"5.50","itemType":"food", "years":[2016,2017]},
@@ -16,7 +18,7 @@ jsonpCallback({"products":[
 	{"id":14,"name":"Yueng. Lager","price":"4.00","itemType":"draft", "years":[2016,2017]},
 	{"id":15,"name":"Blue Moon","price":"4.00","itemType":"bottle", "years":[2016,2017]},
 	{"id":16,"name":"Corona","price":"4.00","itemType":"bottle", "years":[2016,2017]},
-	{"id":25,"name":"Twisted Tea","price":"4.00","itemType":"bottle", "years":[2016,2017]},
+	{"id":25,"name":"Twisted Tea","price":"4.00","itemType":"bottle", "years":[2017]},
 	{"id":18,"name":"Wine","price":"4.00","itemType":"bottle", "years":[2016,2017]},
 	{"id":19,"name":"Coors NA","price":"2.50","itemType":"na", "years":[2016,2017]},
 	{"id":20,"name":"Root Beer","price":"1.50","itemType":"na", "years":[2016,2017]},
@@ -26,6 +28,7 @@ jsonpCallback({"products":[
 	{"id":12,"name":"Guinness Draft","price":"5.50","itemType":"draft", "years":[2016]},
 	{"id":17,"name":"Smmr Shandy","price":"4.00","itemType":"bottle", "years":[2016]},
 	],
+	// universal settings for app
     "sitename":"Pete and Larry's Place",
     "eventname":"St. Charles Carnival",
     "website":"www.scbcarnival.com",
