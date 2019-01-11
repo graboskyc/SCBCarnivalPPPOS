29d989a536c1c085c64d606442a8adf283c8b960
Chris Grabosky <chris@grabosky.net>
2019-01-11 10:33:51 -0500

    Improving analyic capabilities

--- Stitch/services/save/incoming_webhooks/save/source.js
+++ Stitch/services/save/incoming_webhooks/save/source.js
@@ -1,6 +1,8 @@
 exports = async function(payload,response) {
   var recpt = JSON.parse(payload.body.text());
   recpt.created = new Date(Date.now());
+  recpt.isCredit = false;
+  if(recpt.recpt.Te == "Credit") { recpt.isCredit = true; }
   var conn = context.services.get("mongodb-atlas").db("digisign").collection("pos");
   await conn.insertOne(recpt);
 };
