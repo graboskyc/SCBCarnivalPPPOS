cb98119f3f9b853099139f6c8f2919e884ec7592
Chris Grabosky <chris@grabosky.net>
2019-01-28 13:47:00 -0500

    serverside update to sort docs by category (itemtype) then name

--- Stitch/services/save/incoming_webhooks/get/source.js
+++ Stitch/services/save/incoming_webhooks/get/source.js
@@ -4,8 +4,11 @@ exports = async function(payload,response) {
   year = year*1;
   var conn = context.services.get("mongodb-atlas").db("digisign").collection("menu");
   var r = '';
-  var docs = await conn.find({"station":station, "menuyear":year}).toArray()
-  r = JSON.stringify(docs[0]);
+  //var docs = await conn.find({"station":station, "menuyear":year}).toArray()
+  var fulldoc = await conn.aggregate([{$match: {"station":station, "menuyear":year}}]).toArray();
+  var docs = await conn.aggregate([{$match: {"station":station, "menuyear":year}}, {$unwind: {path: "$products",includeArrayIndex: 'index',preserveNullAndEmptyArrays: false}}, {$sort: {"products.itemType":1, "products.name":1}}, {$group: {_id: "$_id",products: {$push:"$products"}}}]).toArray();
+  let mergeddoc = {...fulldoc[0], ...docs[0]}
+  r = JSON.stringify(mergeddoc);
   
   //console.log(r);
   response.setHeader("Content-Type", "application/json");
