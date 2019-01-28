a2ff1bdad73c31f728ae5360410378996238d584
Chris Grabosky <chris@grabosky.net>
2019-01-28 11:14:40 -0500

    2019 menu placeholders

---
+++ products_foodtent.js
@@ -0,0 +1,68 @@
+jsonpCallback({
+    "products": [
+        {
+            "id": 1001,
+            "name": "Hamberder",
+            "price": "3.50",
+            "itemType": "food",
+            "years": [2019]
+        },
+        {
+            "id": 1002,
+            "name": "Hot Dog",
+            "price": "3.50",
+            "itemType": "food",
+            "years": [2019]
+        },
+        {
+            "id": 1003,
+            "name": "Cheese Steak",
+            "price": "5.00",
+            "itemType": "food",
+            "years": [2019]
+        },
+        {
+            "id": 1004,
+            "name": "Chkn Cheese Steak",
+            "price": "5.00",
+            "itemType": "food",
+            "years": [2019]
+        },
+        {
+            "id": 1005,
+            "name": "Hoagie",
+            "price": "5.00",
+            "itemType": "food",
+            "years": [2019]
+        },
+        {
+            "id": 1006,
+            "name": "Pork Roll",
+            "price": "4.50",
+            "itemType": "food",
+            "years": [2019]
+        },
+        {
+            "id": 1007,
+            "name": "Water",
+            "price": "1.50",
+            "itemType": "na",
+            "years": [2019]
+        },
+        {
+            "id": 1008,
+            "name": "Soda",
+            "price": "1.50",
+            "itemType": "na",
+            "years": [2019]
+        },
+    ],
+    "encKey": "ftft",
+    "sitename": "Food Tent",
+    "eventname": "St. Charles Carnival",
+    "website": "www.scbcarnival.com",
+    "eventdate": "June 10-15, 6-11pm nightly",
+    "menuyear": 2019,
+    "station": "scbcarnifoodtent",
+    "stitchposturl": ""
+});
---
+++ products_pp.js
@@ -0,0 +1,194 @@
+jsonpCallback({"products": [
+	// price list, ID not important, just must be unique.
+	// order in list below is order on page
+	{
+        "id": 1,
+        "name": "Clams (6)",
+        "price": "3.50",
+        "itemType": "food",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 2,
+        "name": "Crab Cake",
+        "price": "6.00",
+        "itemType": "food",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 3,
+        "name": "Fried Calamari",
+        "price": "5.50",
+        "itemType": "food",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 4,
+        "name": "Fried Pickles",
+        "price": "4.50",
+        "itemType": "food",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 5,
+        "name": "Fries",
+        "price": "3.00",
+        "itemType": "food",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 6,
+        "name": "Chili Dog",
+        "price": "2.50",
+        "itemType": "food",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 7,
+        "name": "Kraut Dog",
+        "price": "2.00",
+        "itemType": "food",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 8,
+        "name": "Moz Sticks",
+        "price": "4.00",
+        "itemType": "food",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 9,
+        "name": "Onion Rings",
+        "price": "3.50",
+        "itemType": "food",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 10,
+        "name": "Poppers",
+        "price": "4.00",
+        "itemType": "food",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 11,
+        "name": "Wings",
+        "price": "9.00",
+        "itemType": "food",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 28,
+        "name": "Oysters (3)",
+        "price": "5.50",
+        "itemType": "food",
+        "special": true,
+        "years": [2017, 2018, 2019]
+    }, {
+        "id": 23,
+        "name": "Craft",
+        "price": "5.00",
+        "itemType": "draft",
+        "special": true,
+        "years": [2017, 2018, 2019]
+    }, {
+        "id": 13,
+        "name": "Miller Lite",
+        "price": "4.00",
+        "itemType": "draft",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 24,
+        "name": "Smmr Shandy",
+        "price": "4.00",
+        "itemType": "draft",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 14,
+        "name": "Yueng. Lager",
+        "price": "4.00",
+        "itemType": "draft",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 15,
+        "name": "Blue Moon",
+        "price": "4.00",
+        "itemType": "bottle",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 16,
+        "name": "Corona",
+        "price": "4.00",
+        "itemType": "bottle",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 25,
+        "name": "Twisted Tea",
+        "price": "4.00",
+        "itemType": "bottle",
+        "years": [2017, 2018, 2019]
+    }, {
+        "id": 26,
+        "name": "Wine",
+        "price": "4.50",
+        "itemType": "bottle",
+        "years": [2017, 2018, 2019]
+    }, {
+        "id": 19,
+        "name": "Coors NA",
+        "price": "2.50",
+        "itemType": "na",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 20,
+        "name": "Root Beer",
+        "price": "1.50",
+        "itemType": "na",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 21,
+        "name": "Water",
+        "price": "1.50",
+        "itemType": "na",
+        "years": [2016, 2017, 2018, 2019]
+    }, {
+        "id": 29,
+        "name": "Ice",
+        "price": "7.00",
+        "itemType": "util",
+        "years": [2018, 2019]
+	}, 
+	// discontinued menu items
+	{
+        "id": 12,
+        "name": "Guinness Draft",
+        "price": "5.50",
+        "itemType": "draft",
+        "years": [2016]
+    }, {
+        "id": 17,
+        "name": "Smmr Shandy",
+        "price": "4.00",
+        "itemType": "bottle",
+        "years": [2016]
+    }, {
+        "id": 18,
+        "name": "Wine",
+        "price": "4.00",
+        "itemType": "bottle",
+        "years": [2016]
+    }, {
+        "id": 27,
+        "name": "Oysters (1)",
+        "price": "1.50",
+        "itemType": "food",
+        "special": true,
+        "years": [2016]
+    }, {
+        "id": 22,
+        "name": "Ice",
+        "price": "6.00",
+        "itemType": "util",
+        "years": [2016, 2017]
+	}],
+	// universal settings for app
+    "encKey": "B33p",
+    "sitename": "Pete and Larry's Place",
+    "eventname": "St. Charles Carnival",
+    "website": "www.scbcarnival.com",
+    "eventdate": "June 10-15, 6-11pm nightly",
+    "menuyear": 2019,
+    "station": "scbcarnipetesplace",
+    "stitchposturl": ""
+});
+
